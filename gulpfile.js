2e482eadbda7e2eb72beba427e3a006627e36295
党程程 <dangchengcheng@dangchengchengdeMacBook-Pro.local>
2015-09-06 18:09:52 +0800

    build task finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,28 @@
 'use strict';
 
-
 var gulp=require('gulp');
+var watch=require('gulp-watch');
+var concat=require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('jshint',function(){
+    return gulp.src('./src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+gulp.task('compressCss',function(){
+    return gulp.src('./src/css/*.css')
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./dist/css/'));
+});
+gulp.task('compressJs',function(){
+    return gulp.src('./src/js/*.js')
+        .pipe(concat('clippingImageH5.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'));
+});
 gulp.task('default',function(){
-    console.error("李刚你大爷");
+    gulp.watch(['./src/css/*','./src/js/*'],['jshint','compressJs','compressCss']);
 });
---
+++ src/js/clippingImageH5.js
@@ -0,0 +1,7 @@
+var a=1;
+var b=3;
+var c=3;
+var alert=function(msg){
+    console.error("这里有错误"+msg);
+}
+alert("李刚达尔比");
